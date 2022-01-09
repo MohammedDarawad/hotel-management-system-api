709d82e6cb099329c6a285bcdb253056d05bfe26
MohamadAlwan <91935195+MohamadAlwan@users.noreply.github.com>
2022-01-09 20:28:18 +0200

    Add files via upload

---
+++ reception-info.php
@@ -0,0 +1,25 @@
+<?php
+
+		$server_name = "localhost";
+		$username = "root";
+		$password = "";
+		$dbname = "hotel-management-system";
+		
+		$conn = new mysqli($server_name, $username, $password, $dbname);
+		if ($conn->connect_error) {
+			die("Connection failed: " . $conn->connect_error);
+		} 
+		$sql = "select * from users where userType = 1" ;
+		
+		$result = $conn->query($sql);
+		$resultarray = array();
+		while($row =mysqli_fetch_assoc($result))
+		{
+			$resultarray[] = $row;
+		}
+		echo json_encode($resultarray);
+		
+		$conn->close();
+		
+    
+?>
