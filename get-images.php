9668f46065418503a488648463b9ddbf27987170
Mohammed Darawad <moe.darawad@gmail.com>
2022-01-10 01:58:12 +0200

    test

--- get-images.php
+++ get-images.php
@@ -1,7 +1,8 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == "GET") {
-    if (isset($_GET['rId'])) {
+    if (isset($_GET['rId']) && isset($_GET['getAllImages'])) {
         $rId = $_GET['rId'];
+        $getAllImages = $_GET['getAllImages'];
 
         $directory = "images/rooms/" . $rId . "/";
 
@@ -11,11 +12,14 @@ if ($_SERVER['REQUEST_METHOD'] == "GET") {
 
         $i = 1;
         foreach ($images as $image) {
-            $arr = array('URL' => "http://10.0.2.2/".$image);
+            $arr = array('URL' => "http://10.0.2.2/" . $image);
             $resultarray[] = $arr;
         }
-        echo json_encode($resultarray,JSON_UNESCAPED_SLASHES);
-
+        if ($getAllImages == 0) {
+            echo json_encode($resultarray[0], JSON_UNESCAPED_SLASHES);
+        } else {
+            echo json_encode($resultarray, JSON_UNESCAPED_SLASHES);
+        }
     } else {
         die("wrong input");
     }
