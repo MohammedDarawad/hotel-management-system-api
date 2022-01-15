ab3ace601ea0fd65814bb1478c6fd43c984cd1b0
Mohammed Darawad <moe.darawad@gmail.com>
2022-01-16 00:32:01 +0200

    test

--- set-checkIn.php
+++ set-checkIn.php
@@ -14,28 +14,34 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 		die("Connection failed: " . $conn->connect_error);
 	}
 
+	$sql2 = "select rId from reserved where id =" . $id;
+	$result = $conn->query($sql2);
+	$row = mysqli_fetch_assoc($result);
+
 	if ($val == 1) {
 		$sql = "update reserved set isCheckedIn=1 where id=" . $id;
 		if ($conn->query($sql) === TRUE) {
 			$response['hasError'] = false;
 			$response['message'] = "Checked in successfully!";
+
+			$sql4 = "update rooms set isReserved=1 where rId=" . $row["rId"];
+			$conn->query($sql4);
 		} else {
 			$response['hasError'] = true;
 			$response['message'] = "Error, " . $conn->error;
 		}
 		echo json_encode($response);
 	} else {
-		$sql2 = "select rId from reserved where id =" . $id;
-		$result = $conn->query($sql2);
-		$row = mysqli_fetch_assoc($result);
-
-		$sql3 = "update rooms set isReserved=0 where rId=" . $row["rId"];
-		$conn->query($sql3);
-
 		$sql = "delete from reserved where id=" . $id;
 		if ($conn->query($sql) === TRUE) {
 			$response['hasError'] = false;
 			$response['message'] = "Checked out successfully!";
+
+			$sql3 = "update rooms set isReserved=0 where rId=" . $row["rId"];
+			$conn->query($sql3);
+
+			$sql4 = "delete from requestedservices where rId =" . $row["rId"];
+			$conn->query($sql4);
 		} else {
 			$response['hasError'] = true;
 			$response['message'] = "Error, " . $conn->error;
